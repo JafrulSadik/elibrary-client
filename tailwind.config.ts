e57058c687d001a0fb34a5fefe45926efddaf435
SadikMisuk <misukbd4@gmail.com>
2024-07-07 02:50:04 +0600

    add custom color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,37 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        peace: {
+          "400": "#361E1E",
+        },
+        crusta: {
+          "50": "#fff6ed",
+          "100": "#ffebd4",
+          "200": "#ffd2a8",
+          "300": "#ffb271",
+          "350": "#FFA86F",
+          "400": "#ff8434",
+          "500": "#fe6511",
+          "600": "#ef4a07",
+          "700": "#c63408",
+          "800": "#9d2b0f",
+          "900": "#7e2610",
+          "950": "#441006",
+        },
+        "soft-peach": {
+          "50": "#faf6f6",
+          "100": "#f5eeee",
+          "200": "#ebdddd",
+          "300": "#ddc4c4",
+          "400": "#c7a2a2",
+          "500": "#b18282",
+          "600": "#9a6868",
+          "700": "#805555",
+          "800": "#6c4848",
+          "900": "#5c4040",
+          "950": "#301f1f",
+        },
       },
     },
   },
