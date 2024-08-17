b19185092b103b3de7b5b732d9c32063e908fa5d
SadikMisuk <misukbd4@gmail.com>
2024-08-18 01:08:30 +0600

    add response type

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -5,6 +5,18 @@ export type ApiSuccessfullResponse<TData, TPagination> = {
   pagination: TPagination;
 };
 
+export type ApiResponseArryData<TData> = {
+  code: number;
+  message: string;
+  data: TData[];
+};
+
+export type ApiResponseSingleData<TData> = {
+  code: number;
+  message: string;
+  data: TData;
+};
+
 export type AddReviewResponse<TData> = {
   code: number;
   message: string;
