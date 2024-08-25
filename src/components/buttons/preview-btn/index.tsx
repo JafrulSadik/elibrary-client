6d13ac79ddd7bb7eeb3011c4ef19fb2f0046d9e6
SadikMisuk <misukbd4@gmail.com>
2024-08-26 03:30:33 +0600

    style: adjust button design

--- src/components/buttons/preview-btn/index.tsx
+++ src/components/buttons/preview-btn/index.tsx
@@ -2,8 +2,8 @@
 
 const PreviewBtn = () => {
   return (
-    <button className="flex justify-center items-center h-8 w-60 md:h-12 md:w-60 lg:w-60 lg:h-14 rounded-md bg-peace-400 text-white">
-      Read the book
+    <button className="flex justify-center items-center h-8 w-60 md:h-12 md:w-60  lg:w-60 lg:h-14 bg-peace-400 rounded-md text-white">
+      Read Online
     </button>
   );
 };
