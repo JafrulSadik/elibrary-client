3c60757294c7595e511d8c2033aadf17302f6040
SadikMisuk <misukbd4@gmail.com>
2024-08-30 09:50:25 +0600

    feat: add dynamic 'Read Online' and 'Download Book' buttons

--- src/components/buttons/download-btn/index.tsx
+++ src/components/buttons/download-btn/index.tsx
@@ -1,10 +1,22 @@
 "use client";
 
-const DownloadBtn = () => {
+import Link from "next/link";
+
+type Props = {
+  downloadLink: string;
+};
+
+const DownloadBtn = (props: Props) => {
+  const { downloadLink } = props;
   return (
-    <button className="flex justify-center items-center h-8 w-60 md:h-12 md:w-60  lg:w-60 lg:h-14 bg-crusta-950 rounded-md text-white">
+    <Link
+      href={downloadLink}
+      rel="noopener noreferrer"
+      target="_blank"
+      className="flex justify-center items-center h-8 w-60 md:h-12 md:w-60  lg:w-60 lg:h-14 bg-crusta-950 rounded-md text-white"
+    >
       Download Now
-    </button>
+    </Link>
   );
 };
 
--- src/components/buttons/preview-btn/index.tsx
+++ src/components/buttons/preview-btn/index.tsx
@@ -1,10 +1,20 @@
 "use client";
 
-const PreviewBtn = () => {
+import Link from "next/link";
+
+type Props = {
+  bookId: string;
+};
+
+const PreviewBtn = (props: Props) => {
+  const { bookId } = props;
   return (
-    <button className="flex justify-center items-center h-8 w-60 md:h-12 md:w-60  lg:w-60 lg:h-14 bg-peace-400 rounded-md text-white">
+    <Link
+      href={`/reader/${bookId}`}
+      className="flex justify-center items-center h-8 w-60 md:h-12 md:w-60  lg:w-60 lg:h-14 bg-peace-400 rounded-md text-white"
+    >
       Read Online
-    </button>
+    </Link>
   );
 };
 
