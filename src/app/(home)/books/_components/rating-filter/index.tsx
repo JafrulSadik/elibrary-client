e9e3637629b3291fe6a8f052c463dcb66ae388ae
SadikMisuk <misukbd4@gmail.com>
2024-09-05 16:57:36 +0600

    feat: implement filter by rating

--- src/app/(home)/books/_components/rating-filter/index.tsx
+++ src/app/(home)/books/_components/rating-filter/index.tsx
@@ -1,11 +1,54 @@
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { FaRegStar, FaStar } from "react-icons/fa";
 
 const RatingFilter = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const selectedRatings = searchParams.get("ratings")?.split(",");
+
+  const handleRating = (rating: string) => {
+    const params = new URLSearchParams(searchParams);
+
+    const paramRatings = params.get("ratings");
+    let updatedRatings = "";
+
+    if (paramRatings) {
+      const prevRatings = paramRatings.split(",");
+      if (prevRatings.includes(rating)) {
+        const filteredRatings = prevRatings.filter((item) => item !== rating);
+        updatedRatings = filteredRatings.join(",");
+      } else {
+        const updatedGRatingsArray = [...prevRatings, rating];
+        updatedRatings = updatedGRatingsArray.join(",");
+      }
+    } else {
+      updatedRatings = rating;
+    }
+
+    if (updatedRatings) {
+      params.set("ratings", updatedRatings);
+    } else {
+      params.delete("ratings");
+    }
+
+    const url = `${pathname}?${params.toString()}`;
+
+    router.push(url);
+    router.refresh();
+  };
+
   return (
     <div className="text-sm">
       <div>
         <div className="flex items-center px-3 gap-2 py-1">
-          <input type="checkbox" className="accent-peace-400 " />
+          <input
+            onChange={() => handleRating("5")}
+            checked={selectedRatings?.includes("5")}
+            type="checkbox"
+            className="accent-peace-400"
+          />
           <div className="flex text-crusta-400">
             <FaStar />
             <FaStar />
@@ -18,7 +61,12 @@ const RatingFilter = () => {
       </div>
       <div>
         <div className="flex items-center px-3 gap-2 py-1">
-          <input type="checkbox" className="accent-peace-400 " />
+          <input
+            onChange={() => handleRating("4")}
+            checked={selectedRatings?.includes("4")}
+            type="checkbox"
+            className="accent-peace-400 "
+          />
           <div className="flex text-crusta-400">
             <FaStar />
             <FaStar />
@@ -31,7 +79,12 @@ const RatingFilter = () => {
       </div>
       <div>
         <div className="flex items-center px-3 gap-2 py-1">
-          <input type="checkbox" className="accent-peace-400 " />
+          <input
+            onChange={() => handleRating("3")}
+            checked={selectedRatings?.includes("3")}
+            type="checkbox"
+            className="accent-peace-400 "
+          />
           <div className="flex text-crusta-400">
             <FaStar />
             <FaStar />
@@ -44,7 +97,12 @@ const RatingFilter = () => {
       </div>
       <div>
         <div className="flex items-center px-3 gap-2 py-1">
-          <input type="checkbox" className="accent-peace-400 " />
+          <input
+            onChange={() => handleRating("2")}
+            checked={selectedRatings?.includes("2")}
+            type="checkbox"
+            className="accent-peace-400 "
+          />
           <div className="flex text-crusta-400">
             <FaStar />
             <FaStar />
@@ -57,7 +115,12 @@ const RatingFilter = () => {
       </div>
       <div>
         <div className="flex items-center px-3 gap-2 py-1">
-          <input type="checkbox" className="accent-peace-400 " />
+          <input
+            onChange={() => handleRating("1")}
+            checked={selectedRatings?.includes("1")}
+            type="checkbox"
+            className="accent-peace-400 "
+          />
           <div className="flex text-crusta-400">
             <FaStar />
             <FaRegStar />
